b3af8a79ab28b276b85ad2833c25aa38e1545ca6
Alex Tilatti <alextilatti@Alexs-MacBook-Pro.local>
2025-08-20 14:21:46 -0400

    Initial commit of new project

---
+++ backend/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    db_user: str
+    db_password: str
+    db_host: str
+    db_port: int
+    db_name: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+
+DATABASE_URL = (
+    f"postgresql+psycopg2://{settings.db_user}:{settings.db_password}"
+    f"@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+)
