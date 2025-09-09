d38518a83241b46e6723f8db7a6d96e36e75e95d
Alex Tilatti <alex.tilatti.relay@gmail.com>
2025-09-09 14:38:18 -0400

    fix for layout on pages + providers refactor

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import AppBar from "./components/AppBar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,10 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <AppRouterCacheProvider>
-          <Providers>{children}</Providers>
-        </AppRouterCacheProvider>
+      <body>
+        <Providers>
+          <AppBar />
+          <main> {children} </main>
+        </Providers>
       </body>
     </html>
   );
