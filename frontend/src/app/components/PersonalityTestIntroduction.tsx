5789051e61006625c6d628b81d84e780ae493924
Alex Tilatti <alex.tilatti.relay@gmail.com>
2025-12-18 09:57:48 -0500

    add hook for getting the personality scores

--- frontend/src/app/components/PersonalityTestIntroduction.tsx
+++ frontend/src/app/components/PersonalityTestIntroduction.tsx
@@ -5,16 +5,22 @@ import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import StyledCard from "@/app/components/common/StyledCard";
 import { useRouter } from "next/navigation";
+import { useCreatePersonalityTest } from "../api/scores/scores";
 
 const PersonalityTestIntroduction = () => {
   const router = useRouter();
+  const { mutate: personalityTestMutate } = useCreatePersonalityTest({
+    onSuccess: () => {
+      router.push("/personality-tests");
+    },
+  });
+
   return (
     <>
       <Typography variant="h3">Personality Test Start</Typography>
       <StyledCard
         onClick={() => {
-          console.log("clicked");
-          router.push("/personality-tests");
+          personalityTestMutate();
         }}
         variant="outlined"
         data-testid="myself-tile"
